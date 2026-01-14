329e6e60ffda1435d3f6b565396134bc38b61bf0
debanshu <debanshupanigrahi536052gmail.com>
2026-01-14 22:44:53 +0530

    chore: add reqCtx within ctx

--- src/core/types/types.ts
+++ src/core/types/types.ts
@@ -6,24 +6,37 @@ import type { STATE } from "../state/state.ts";
 import type { Phase } from "../phase/Phase.ts";
 
 export type ProxyContext = {
-  req?: IncomingMessage;
-  res?: ServerResponse;
-  upstreamRes?: IncomingMessage;
+  id: string;
   socket?: Stream.Duplex | Socket;
   tlsSocket?: TLSSocket;
   head?: any;
   err?: Error;
-  state: Map<State | string, any>;
+  conn_state: Map<string, any>;
+  conn_type?: "tcp" | "http" | "https";
+  reqCtx: {
+    id: string;
+    // conn_id: string;
+    req?: IncomingMessage;
+    res?: ServerResponse;
+    upstreamRes?: IncomingMessage | any | undefined;
+    state: Map<State | string, any>;
+    /**
+     * @experimental
+     * current_phase?: Phase;
+     */
+    // current_phase?: Phase;
+    next_phase?: Phase;
+    // next_handler?: Handler;
+  };
 };
 export type State = (typeof STATE)[keyof typeof STATE];
 export type Handler = {
-  name: string;
+  // name: string;
   phase: Phase;
-  order: number;
-  execute(ctx: ProxyContext): Promise<void>;
-  register(): void;
-  unregister(): void;
-  isRegistered(): boolean;
+  handle(ctx: ProxyContext): Promise<void>;
+  // register(): void;
+  // unregister(): void;
+  // isRegistered(): boolean;
 };
 
 export type Destroyable = {
