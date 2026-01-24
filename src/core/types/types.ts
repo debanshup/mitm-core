a1f5046ddb18a2729c86d2cb03c7d76693cdc2f0
debanshu <debanshupanigrahi536052gmail.com>
2026-01-24 19:46:37 +0530

    types: add CachedResponse type

--- src/core/types/types.ts
+++ src/core/types/types.ts
@@ -1,32 +1,22 @@
-import type { Socket } from "net";
-import type { IncomingMessage, ServerResponse } from "http";
-import type Stream from "stream";
-import type { TLSSocket } from "tls";
+import type { ClientRequest, IncomingMessage, ServerResponse } from "http";
 import type { STATE } from "../state/state.ts";
 import type { Phase } from "../phase/Phase.ts";
 
 export type ProxyContext = {
   id: string;
-  socket?: Stream.Duplex | Socket;
-  tlsSocket?: TLSSocket;
+  // socket?: Stream.Duplex | Socket;
+  // tlsSocket?: TLSSocket;
   head?: any;
   err?: Error;
   conn_state: Map<string, any>;
   conn_type?: "tcp" | "http" | "https";
   reqCtx: {
     id: string;
-    // conn_id: string;
     req?: IncomingMessage;
     res?: ServerResponse;
-    upstreamRes?: IncomingMessage | any | undefined;
+    upstream?: ClientRequest,
     state: Map<State | string, any>;
-    /**
-     * @experimental
-     * current_phase?: Phase;
-     */
-    // current_phase?: Phase;
     next_phase?: Phase;
-    // next_handler?: Handler;
   };
 };
 export type State = (typeof STATE)[keyof typeof STATE];
@@ -43,4 +33,11 @@ export type Destroyable = {
   destroyed?: boolean;
   destroy: (error?: Error) => void;
   end?: () => void;
+};
+
+export type CachedResponse = {
+  status: number;
+  headers: Record<string, string | string[] | undefined>;
+  body: Buffer;
+  expires: number;
 };
