81de4f0d17a11da1f131cf9c42c7e6beb9d1400a
debanshu <debanshupanigrahi536052gmail.com>
2026-01-30 23:30:23 +0530

    refactor: clean up tunnel logic

--- src/core/direct-tunnel/Tunnel.ts
+++ src/core/direct-tunnel/Tunnel.ts
@@ -1,25 +1,11 @@
 import type { ProxyContext } from "../types/types.ts";
-import { BYPASS_RULES } from "./rules.ts";
-import http from "http";
 import net from "net";
 export class Tunnel {
   protected constructor() {}
-  static shouldBypass(req: http.IncomingMessage): boolean {
-    // console.info(BYPASS_RULES);
-    const host = req.url?.split(":")[0]!;
-    // console.info(host);
-    const result = BYPASS_RULES.some((r) => r.test(host));
-    return result;
-  }
-
   static async createDirectTunnel(ctx: ProxyContext) {
-    //   const { req, res, socket } = ctx;
     console.info("Direct tunnel to", ctx.reqCtx.req?.headers.host);
-
-    // const reqCtx = ctx.reqCtx
     const req = ctx.reqCtx.req;
     const socket = req?.socket!
-
     const hostHeader = req!.headers.host!;
     const [host, portStr] = hostHeader.split(":");
     const port = Number(portStr) || 443;
