50fbb0758db4ca4ee44f72f92098519a1d8733d8
debanshup <debanshupanigrahi53605@gmail.com>
2026-04-03 23:18:44 +0530

    modified:   test/Proxy.ts

--- test/Proxy.ts
+++ test/Proxy.ts
@@ -1,26 +1,26 @@
 import Proxy from "../src/dist/Proxy.ts";
-(await Proxy.registerMiddleware()).initPipelines();
+await Proxy.registerMiddleware({ initializePipelines: true });
 const proxy = new Proxy();
 setInterval(() => {
-  const mem = process.memoryUsage();
-  console.log("Heap Used:", mem.heapUsed / 1024 / 1024, "MB");
+  // const mem = process.memoryUsage();
+  // console.log("Heap Used:", mem.heapUsed / 1024 / 1024, "MB");
 }, 1000);
 
-proxy.onTCPconnection((socket, next) => {
+proxy.onTCPconnection((socket, defaultHandler) => {
   // console.time("TCP")
-  next();
+  defaultHandler();
   // console.timeEnd("TCP")
 });
-proxy.onConnect((req, socket, head, next) => {
+proxy.onConnect((req, socket, head, defaultHandler) => {
   // console.time("CONNECT for "+req.url);
-  next();
+  defaultHandler();
   // console.timeEnd("CONNECT for "+req.url);
 });
 
-proxy.onRequest((req, res, next) => {
+proxy.onRequest((req, res, defaultHandler) => {
   // console.time("_REQ");
 
-  next();
+  defaultHandler();
   // console.timeEnd("_REQ");
 });
 
