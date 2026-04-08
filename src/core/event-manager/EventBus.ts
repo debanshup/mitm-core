688d4a80150e1fa902dfe6bc1e125ab36799c12d
debanshup <debanshupanigrahi53605@gmail.com>
2026-04-08 21:41:38 +0530

    refactor(event-bus): enhance eventBus and override additional core methods for improved lifecycle control

--- src/core/event-manager/EventBus.ts
+++ src/core/event-manager/EventBus.ts
@@ -1,22 +1,23 @@
 import EventEmitter from "events";
-// export const connectionEvents = new EventEmitter();
-
-// export const ConnectionTypes = {
-//     TCP:"tcp",
-//     HTTP : "http",
-//     CONNECT : "connect",
-//     DECRYPTED_HTTP :"decrypted_http"
-// }
-
-
 
 export class TypedEventEmitter<T extends object> extends EventEmitter {
-  
   override on<K extends keyof T & (string | symbol)>(
     event: K,
-    listener: T[K] extends any[] ? (...args: T[K]) => void : never
+    listener: T[K] extends any[] ? (...args: T[K]) => void : never,
   ): this {
     return super.on(event, listener as any);
+  }
+  override once<K extends keyof T & (string | symbol)>(
+    event: K,
+    listener: T[K] extends any[] ? (...args: T[K]) => void : never,
+  ): this {
+    return super.once(event, listener as any);
+  }
+  override off<K extends keyof T & (string | symbol)>(
+    event: K,
+    listener: T[K] extends any[] ? (...args: T[K]) => void : never,
+  ): this {
+    return super.off(event, listener as any);
   }
 
   override emit<K extends keyof T & (string | symbol)>(
@@ -24,6 +25,14 @@ export class TypedEventEmitter<T extends object> extends EventEmitter {
     ...args: T[K] extends any[] ? T[K] : []
   ): boolean {
     return super.emit(event, ...args);
+  }
+
+  override listeners<K extends keyof T & (string | symbol)>(
+    event: K,
+  ): Array<T[K] extends any[] ? (...args: T[K]) => void : never> {
+    return super.listeners(event as string | symbol) as Array<
+      T[K] extends any[] ? (...args: T[K]) => void : never
+    >;
   }
 
   override removeAllListeners(event?: keyof T & (string | symbol)): this {
