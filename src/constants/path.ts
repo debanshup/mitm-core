ab63008a16364cb187c81ab34190adcf0cfc56d9
debanshup <debanshupanigrahi53605@gmail.com>
2026-04-08 21:37:37 +0530

    modified:   src/constants/path.ts

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -1,5 +1,9 @@
-import os from "os"
-import path from "path"
-export const PATH = {
-    CERT_DIR: path.join(os.homedir(),".mitm-core")
-}
+import os from "os";
+import path from "path";
+export const LEAF_PATH = {
+  CERT_DIR: path.join(os.homedir(), ".mitm-core"),
+};
+
+export const CA_PATH = {
+  CA_DIR: path.join(process.cwd(), "creds/__self__"),
+};
