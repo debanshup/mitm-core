c70183f397c4b3762eac1164a7555400127bb856
debanshup <debanshupanigrahi53605@gmail.com>
2026-04-08 21:53:15 +0530

    refactor(request-parser): improve request parser performance and accuracy

--- src/utils/parser/parseReqData.ts
+++ src/utils/parser/parseReqData.ts
@@ -1,28 +1,44 @@
 import * as http from "http";
-// export function parseHttpReqData(req: http.IncomingMessage) {
-//   const hostHeader = req.headers.host;
-
-//   if (!hostHeader) {
-//     throw new Error("Missing Host header");
-//   }
-
-//   const [host, portFromHost] = hostHeader.split(":");
-
-//   const isTLS = (req.socket as any).encrypted === true;
-
-//   return {
-//     host,
-//     port: portFromHost ? Number(portFromHost) : isTLS ? 443 : 80,
-//     method: req.method,
-//     headers: req.headers,
-//     path: req.url,
-//   };
-// }
-
+import tls from "tls";
 export function parseConnectData(req: http.IncomingMessage) {
+  if (!req || !req.url) {
+    return {
+      host: "",
+      port: null,
+      url: "",
+    };
+  }
   const [host, port] = req.url!.split(":");
   return {
-    host,
+    host: host!,
     port: port ? Number(port) : 443,
+    url: req.url!,
+  };
+}
+
+export function parseHttpRequestData(
+  req: http.IncomingMessage,
+  forceEncrypted?: boolean,
+) {
+  const isEncrypted =
+    forceEncrypted ??
+    (req.socket instanceof tls.TLSSocket || (req.socket as any).encrypted);
+
+  const protocol = isEncrypted ? "https:" : "http:";
+  const defaultPort = isEncrypted ? 443 : 80;
+
+  const rawUrl = req.url!.startsWith("http")
+    ? req.url!
+    : `${protocol}//${req.headers.host}${req.url}`;
+
+  const parsedUrl = new URL(rawUrl);
+
+  return {
+    protocol: parsedUrl.protocol,
+    host: parsedUrl.hostname,
+    port: parsedUrl.port ? Number(parsedUrl.port) : defaultPort,
+    path: parsedUrl.pathname + parsedUrl.search,
+    fullUrl: rawUrl,
+    isEncrypted,
   };
 }
