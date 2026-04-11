dcdceef3a564f0bb2ec5f97b53aad526971b82ab
debanshup <debanshupanigrahi53605@gmail.com>
2026-04-11 23:10:24 +0530

    modified:   src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,13 @@ export type {
   ProxyContext,
   State,
 } from "./types/types.ts";
+export type { IRuleParser } from "./core/rule-manager/parser.ts";
 export { Phase } from "./phase/Phase.ts";
 export { BasePlugin } from "./plugins/base/BasePlugin.ts";
 export { PluginRegistry } from "./plugins/PluginRegistry.ts";
 export { Middleware } from "./middleware/middleware.ts";
 export { Proxy } from "./core/Proxy.ts";
 export { RuleEngine } from "./core/rule-manager/RuleEngine.ts";
-export type { IRuleParser } from "./core/rule-manager/parser.ts";
+export {Tunnel} from "./core/direct-tunnel/Tunnel.ts"
+export {PipelineAbortSignal} from "./core/signals/pipelineAbortSignal.ts"
 // export { WatchableRuleFile } from "./core/rule-manager/ruleStore.ts";
