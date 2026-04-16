e15378f4b2674481b44b640cf25c51a44b8bff9f
debanshup <debanshupanigrahi53605@gmail.com>
2026-04-16 12:19:14 +0530

    refactor(middleware): streamline execution flow for better performance and control

--- src/middleware/middleware.ts
+++ src/middleware/middleware.ts
@@ -1,11 +1,8 @@
-connectionEvents;
 import { ContextManager } from "../core/context-manager/ContextManager.ts";
 import Pipeline from "../core/pipelines/PipelineCompiler.ts";
-import { Phase } from "../phase/Phase.ts";
-import { STATE } from "../core/state/state.ts";
-import { ProxyUtils } from "../core/utiils/ProxyUtils.ts";
+import { ProxyUtils } from "../core/utils/ProxyUtils.ts";
 import { connectionEvents } from "../core/event-manager/connection-events/connectionEvents.ts";
-import { payloadEvents } from "../core/event-manager/data-events/payloadEvents.ts";
+import { payloadEvents } from "../core/event-manager/payload-events/payloadEvents.ts";
 import {
   parseConnectData,
   parseHttpRequestData,
@@ -18,19 +15,17 @@ export class Middleware {
     initializePipelines: boolean;
   }) {
     if (initializePipelines) {
+      Pipeline.compile();
     }
-    Pipeline.compile();
     connectionEvents.on("TCP", ({ socket }) => {
       // disable nagle's at tcp level
       socket.setNoDelay(true);
       const ctx = ContextManager.getContext(socket);
-      ctx.connectionState.set("conn", "tcp");
-
       socket.on("error", async (err: any) => {
         ProxyUtils.cleanUp([socket]);
         ctx.error = err;
         if (ctx.requestContext) {
-          ctx.requestContext.state.set(STATE.is_error, true);
+          ctx.requestContext.state.set("error", true);
         }
         const isExpectedDrop =
           err.code === "ECONNABORTED" ||
@@ -48,16 +43,16 @@ export class Middleware {
       socket.on("close", () => {
         ProxyUtils.cleanUp([socket]);
         if (ctx.requestContext) {
-          ctx.requestContext.state.set(STATE.is_finished, true);
+          ctx.requestContext.state.set("isFinished", true);
         }
       });
     });
     connectionEvents.on("HTTP:PLAIN", async ({ req, res }) => {
       const ctx = ContextManager.getContext(req.socket);
       ctx.connectionType = "http";
+      ctx.requestContext.nextPhase = "request";
       ctx.requestContext!.req = req;
       ctx.requestContext!.res = res;
-      ctx.requestContext.nextPhase = Phase.REQUEST;
       const { host, fullUrl } = parseHttpRequestData(req);
       // client to proxy
       ctx.clientToProxyHost = host;
@@ -73,9 +68,9 @@ export class Middleware {
     connectionEvents.on("CONNECT", async ({ req, socket, head }) => {
       const ctx = ContextManager.getContext(socket);
       ctx.connectionType = "https";
+      ctx.requestContext.nextPhase = "handshake";
       ctx.head = head;
       ctx.requestContext!.req = req;
-      ctx.requestContext.nextPhase = Phase.HANDSHAKE;
 
       const { host, url } = parseConnectData(req);
       ctx.clientToProxyHost = host;
@@ -87,16 +82,14 @@ export class Middleware {
 
     connectionEvents.on("HTTPS:DECRYPTED", async ({ ctx }) => {
       // console.info("dec https fired!")
-      const { host, path, fullUrl } = parseHttpRequestData(
+      const { host, fullUrl } = parseHttpRequestData(
         ctx.requestContext.req!,
         true,
       );
 
-      // console.info(host, fullUrl)
-
       ctx.proxyToUpstreamHost = host;
       ctx.proxyToUpstreamUrl = fullUrl;
-      ctx.requestContext.nextPhase = Phase.REQUEST;
+      ctx.requestContext.nextPhase = "request";
       await Promise.all(
         payloadEvents
           .listeners("PAYLOAD:REQUEST")
