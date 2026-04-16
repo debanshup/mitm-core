8c426d267dfb20f5fe004b6c29cc48a660de9464
debanshup <debanshupanigrahi53605@gmail.com>
2026-04-16 11:55:53 +0530

    deleted:    src/core/direct-tunnel/Tunnel.ts 	deleted:    src/core/event-manager/data-events/payloadEvents.ts 	deleted:    src/core/event-manager/tls-events/tlsLifecycleEvents.ts 	deleted:    src/core/rule-manager/parser.ts 	deleted:    src/core/state/state.ts 	deleted:    src/utils/upstream/httpUpstream.ts 	deleted:    src/utils/upstream/httpsUpstream.ts 	deleted:    test/Proxy.ts 	deleted:    test/stress.test.ts

--- src/core/event-manager/data-events/payloadEvents.ts
+++
@@ -1,9 +0,0 @@
-import type { ProxyContext } from "../../../types/types.ts";
-import { TypedEventEmitter } from "../EventBus.ts";
-
-export interface PayloadEventMap {
-  "PAYLOAD:REQUEST": [payload: { ctx: ProxyContext }];
-  "PAYLOAD:RESPONSE": [payload: { ctx: ProxyContext }];
-}
-
-export const payloadEvents = new TypedEventEmitter<PayloadEventMap>();
--- src/core/state/state.ts
+++
@@ -1,7 +0,0 @@
-export const STATE = {
-  cert_cache_hit: Boolean,
-  response_cache_hit:Boolean,
-  req_cache_hit: Boolean,
-  is_finished: Boolean,
-  is_error: Boolean,
-};
