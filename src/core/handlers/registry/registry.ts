0e6c212494da0924e29313339467b18fa7d883b5
debanshup <debanshupanigrahi53605@gmail.com>
2026-04-16 12:14:58 +0530

    modified:   src/core/handlers/registry/registry.ts

--- src/core/handlers/registry/registry.ts
+++ src/core/handlers/registry/registry.ts
@@ -1,3 +1,10 @@
-import  * as handlers from "./index.ts"
-// console.info(handlers)
-export const HANDLERS = new Set(Object.values(handlers))
+import type { BaseHandler } from "../base/base.handler.ts";
+import * as handlers from "./index.ts";
+export type HandlerConstructor = new () => BaseHandler;
+
+export const HANDLERS = new Set<HandlerConstructor>(
+  Object.values(handlers).filter((exportItem) => {
+    //  only grab actual classes/functions
+    return typeof exportItem === "function";
+  }) as unknown as HandlerConstructor[],
+);
