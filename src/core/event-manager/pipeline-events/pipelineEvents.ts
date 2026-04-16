9b689f769f423cae353414c03acbaa5d34089437
debanshup <debanshupanigrahi53605@gmail.com>
2026-04-16 12:07:21 +0530

    modified:   src/core/event-manager/pipeline-events/pipelineEvents.ts

--- src/core/event-manager/pipeline-events/pipelineEvents.ts
+++ src/core/event-manager/pipeline-events/pipelineEvents.ts
@@ -2,10 +2,10 @@
  * @experimental : will be used later if needed
  */
 
-import type { ProxyContext } from "../../../types/types.ts";
+import type { ProxyContext } from "../../context-manager/ContextManager.ts";
 import { TypedEventEmitter } from "../EventBus.ts";
 
-interface PipelineEventMap {
+export interface PipelineEventMap {
   PAUSE: [payload: { ctx: ProxyContext }];
   RESUME: [payload: { ctx: ProxyContext }];
   STOP: [payload: { ctx: ProxyContext }];
