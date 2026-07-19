b3fbc1868d388ed5ce766d46bf3360e11946ba4f
debanshup <hello@debanshupanigrahi.dev>
2026-07-19 14:22:56 +0530

    deleted:    src/constants/path.ts

--- src/constants/path.ts
+++
@@ -1,9 +0,0 @@
-import os from "os";
-import path from "path";
-export const LEAF_PATH = {
-  CERT_DIR: path.join(os.homedir(), ".mitm-core"),
-};
-
-export const CA_PATH = {
-  CA_DIR: path.join(process.cwd(), "creds/__self__"),
-};
