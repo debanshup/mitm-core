bcac5e3d6afd94a4bc6d589d42e1ed98ac8482d1
debanshup <hello@debanshupanigrahi.dev>
2026-07-19 14:44:27 +0530

    feat(exports): expose additional types and modules for greater extensibility

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-export type { ProxyContext } from "./core/context-manager/ContextManager";
+export type { RequestScope, RequestContext, RequestLifecycle, SessionContext } from "./core/context-manager/types";
+export type { AbortMessage } from "./core/signals/types";
 export type { IRuleParser } from "./core/rule-manager/ruleStore";
+export type { IProxy } from "./lib/Proxy";
 export { BasePlugin } from "./core/plugin-manager/BasePlugin";
-export { RuleEngine } from "./core/rule-manager/ruleEngine";
+export { RuleEngine } from "./core/rule-manager/RuleEngine";
 export { PipelineAbortSignal } from "./core/signals/pipelineAbortSignal";
-export type { AbortMessage } from "./core/signals/pipelineAbortSignal";
-export { Proxy } from "./core/Proxy";
-export type { IProxy } from "./core/Proxy";
+export { Proxy } from "./lib/Proxy";
