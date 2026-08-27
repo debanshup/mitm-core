ca585f917b486f7ea13c05586b739672c2b2e109
debanshup <hello@debanshupanigrahi.dev>
2026-08-27 22:01:22 +0530

    modified:   src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-export type { RequestScope, RequestContext, RequestLifecycle, SessionContext } from "./core/context-manager/types";
+export type { RequestScope, RequestContext, RequestLifecycle, SessionContext } from "./core/scope/types";
 export type { AbortMessage } from "./core/signals/types";
-export type { IRuleParser } from "./core/rule-manager/ruleStore";
+export type { IRuleParser } from "./core/rule/ruleStore";
 export type { IProxy } from "./lib/Proxy";
-export { BasePlugin } from "./core/plugin-manager/BasePlugin";
-export { RuleEngine } from "./core/rule-manager/RuleEngine";
+export { BasePlugin } from "./core/plugin/BasePlugin";
+export { RuleEngine } from "./core/rule/RuleEngine";
 export { PipelineAbortSignal } from "./core/signals/pipelineAbortSignal";
 export { Proxy } from "./lib/Proxy";
